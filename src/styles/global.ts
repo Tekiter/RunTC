a69cd1a223fd18864cbf264f5960198be19ba8e9
Tekiter <tekiter8@gmail.com>
2022-09-07 22:29:50 +0900

    feat: 폰트 추가

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -6,9 +6,51 @@ import { color } from "./color";
 export const globalStyle = css`
   ${emotionReset};
 
+  @font-face {
+    font-family: "Spoqa Han Sans Neo";
+    font-weight: 100;
+    src: local("Spoqa Han Sans Neo Thin"),
+      url("/fonts/SpoqaHanSansNeo-Thin.woff2") format("woff2");
+  }
+
+  @font-face {
+    font-family: "Spoqa Han Sans Neo";
+    font-weight: 700;
+    src: local("Spoqa Han Sans Neo Bold"),
+      url("/fonts/SpoqaHanSansNeo-Bold.woff2") format("woff2");
+  }
+
+  @font-face {
+    font-family: "Spoqa Han Sans Neo";
+    font-weight: 300;
+    src: local("Spoqa Han Sans Neo Light"),
+      url("/fonts/SpoqaHanSansNeo-Light.woff2") format("woff2");
+  }
+
+  @font-face {
+    font-family: "Spoqa Han Sans Neo";
+    font-weight: 400;
+    src: local("Spoqa Han Sans Neo Regular"),
+      url("/fonts/SpoqaHanSansNeo-Regular.woff2") format("woff2");
+  }
+
+  @font-face {
+    font-family: "Spoqa Han Sans Neo";
+    font-weight: 500;
+    src: local("Spoqa Han Sans Neo Medium"),
+      url("/fonts/SpoqaHanSansNeo-Medium.woff2") format("woff2");
+  }
+
   * {
     box-sizing: border-box;
     font-size: 100%;
+  }
+
+  body,
+  button,
+  input,
+  textarea {
+    font-family: "Spoqa Han Sans Neo", "sans-serif";
   }
 
   :root {
