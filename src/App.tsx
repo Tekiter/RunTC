c29ea4aa5e3aa1c18823dde26b1d4c1d7f20e048
Tekiter <tekiter8@gmail.com>
2022-09-07 12:49:36 +0900

    feat: 글로벌 css 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
 import "@blueprintjs/core/lib/css/blueprint.css";
 
-import { css, Global } from "@emotion/react";
-import emotionReset from "emotion-reset";
+import { Global } from "@emotion/react";
 import { RecoilRoot } from "recoil";
 
 import CodeRunner from "./CodeRunner";
+import { globalStyle } from "./styles/global";
 
 const App: React.FC = () => {
   return (
     <RecoilRoot>
-      <Global
-        styles={css`
-          ${emotionReset}
-        `}
-      />
+      <Global styles={globalStyle} />
       <CodeRunner />
     </RecoilRoot>
   );
